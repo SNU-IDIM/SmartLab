0d5f532989d4d2523a8865504bca5b7fd571c960
WonJae Yun <wonjae.yun@gmail.com>
2019-11-22 17:08:18 +0900

    snu_object_tracker.py 개발중(버그 수정) (19.11.22)

--- src/snu_object_tracker.py
+++ src/snu_object_tracker.py
@@ -18,6 +18,9 @@ RAD2DEG = 180.0 / math.pi
 MM2M = 1.0 / 1000.0
 M2MM  = 1000.0
 
+AR_MARKER_FRAME_PREFIX_ = 'ar_marker_'
+AR_TARGET_FRAME_PREFIX_ = 'ar_target_'
+
 
 OFFSET_FROM_TARGET = 175.0 * MM2M # [mm]
 
@@ -27,27 +30,30 @@ class snu_object_tracker():
         
         rospy.set_param("snu_object_tracker/reference_frame", 'base_0')
         rospy.set_param("snu_object_tracker/object_frame", 'ar_marker_1')
-        rospy.set_param("snu_object_tracker/target_frame", 'target_1')
+        rospy.set_param("snu_object_tracker/target_frame", 'ar_target_1')
 
         self.listener = tf.TransformListener()
         self.broadcaster = tf2_ros.StaticTransformBroadcaster()
         self.static_transformStamped = TransformStamped()
 
         ### Class Variables ###
-        self.reference_frame_name = rospy.get_param("snu_object_tracker/reference_frame", 'base_0')
-        self.object_frame_name    = rospy.get_param("snu_object_tracker/object_frame", 'ar_marker_1')
-        self.target_frame_name    = rospy.get_param("snu_object_tracker/target_frame", 'target_1')
+        self.reference_frame_name = rospy.get_param("snu_object_tracker/reference_frame")
+        self.object_frame_name    = rospy.get_param("snu_object_tracker/object_frame")
+        self.target_frame_name    = rospy.get_param("snu_object_tracker/target_frame")
         self.tags = AlvarMarkers()
         self.cmd_pose = Pose()
 
         ### Topics to Subscribe ###
         rospy.Subscriber('ur_pnp', String, self.pnp_cb, queue_size=1) # Trigger Topic
-        rospy.Subscriber('ar_pose_marker', AlvarMarkers, self.object_sub_cb, queue_size=1) # Object Pose Topic
+        rospy.Subscriber('ar_pose_marker', AlvarMarkers, self.ar_sub_cb, queue_size=1) # Object Pose Topic
         
         ### Topics to Publish ###
         self.pub1 = rospy.Publisher('cmd_moveit', Pose, queue_size=1) # Final target pose to be tracked
+    
 
-
+    '''
+        변환: (trans, rot) -> geometry_msgs/Pose
+    '''
     def update_cmd_pose(self, trans, rot):
         self.cmd_pose.position.x    = trans[0] - 0.020 # 보정 @(arm -> 측면)
         self.cmd_pose.position.y    = trans[1]# - 0.020 # 보정 @(arm -> 정면)
@@ -55,19 +61,54 @@ class snu_object_tracker():
         self.cmd_pose.orientation.x = rot[0]
         self.cmd_pose.orientation.y = rot[1]
         self.cmd_pose.orientation.z = rot[2]
-        self.cmd_pose.orientation.w = rot[3]
+        self.cmd_pose.orientation.w = rot[3]  
 
 
+    '''
+        AR_Marker -> Subscribe -> Target Pose 계산 -> TF Broadcast
+    '''
+    def ar_sub_cb(self, msg):
+        n_tags = len(msg.markers)
+        print "Number of detected tags: %d"%n_tags
+
+        if n_tags is not 0:
+            idx = 0
+            for x in msg.markers:
+                self.static_transformStamped.header.stamp    = rospy.Time.now()
+                self.static_transformStamped.header.frame_id = AR_MARKER_FRAME_PREFIX_ + str(msg.markers[idx].id)
+                self.static_transformStamped.child_frame_id  = AR_TARGET_FRAME_PREFIX_ + str(msg.markers[idx].id)
+            
+                self.static_transformStamped.transform.translation.x = 0.0
+                self.static_transformStamped.transform.translation.y = 0.0
+                self.static_transformStamped.transform.translation.z = OFFSET_FROM_TARGET
+            
+                quat = tf.transformations.quaternion_from_euler(math.pi, 0.0, math.pi/2.0)
+                self.static_transformStamped.transform.rotation.x = quat[0]
+                self.static_transformStamped.transform.rotation.y = quat[1]
+                self.static_transformStamped.transform.rotation.z = quat[2]
+                self.static_transformStamped.transform.rotation.w = quat[3]
+                #print "%s"%self.static_transformStamped
+
+                self.broadcaster.sendTransform(self.static_transformStamped)
+
+                idx += 1
+            return 1
+        else:
+            return -1
+
+
+    '''
+        "/ur_pnp" Subscribe -> if) data == "search" -> TF Subscribe -> "cmd_pose" Publish
+    '''
     def pnp_cb(self, msg): # Version 2: "/tf" topic 이용
-        print "/ur_pnp -> %s"%msg.data
+        print "Subscribed '/ur_pnp' -> '%s'"%msg.data
         if msg.data == 'search':
             # Update the Target Frame Name (임시로 rosparam 이용해 target_frame_name 수정 - SMACH 구성 후 수정 필요)
-            self.object_frame_name = rospy.get_param("snu_object_tracker/object_frame_name")
-            print "Searching for the target: %s ..."%self.target_object_frame_name
+            self.target_frame_name = rospy.get_param("snu_object_tracker/target_frame")
             try:
-                print(self.target_object_frame_name)
-                self.listener.waitForTransform(self.reference_frame_name, self.object_frame_name, rospy.Time(), rospy.Duration(0.5))
-                (trans,rot) = self.listener.lookupTransform(self.reference_frame_name, self.object_frame_name, rospy.Time(0))
+                print "Trying to search the target: %s ..."%self.object_frame_name
+                self.listener.waitForTransform(self.reference_frame_name, self.target_frame_name, rospy.Time(), rospy.Duration(0.5))
+                (trans,rot) = self.listener.lookupTransform(self.reference_frame_name, self.target_frame_name, rospy.Time(0))
 
                 self.update_cmd_pose(trans, rot)
                 self.pub1.publish(self.cmd_pose)
@@ -79,38 +120,6 @@ class snu_object_tracker():
             return 1
         else:
             return -1
-        
-
-    def object_sub_cb(self, msg): # 받는 msg 바꿔야 함
-        n_tags = len(msg.markers)
-        print "Number of detected tags: %d"%n_tags
-
-        if n_tags is not 0:
-            idx = 0
-            for x in msg.markers:
-                self.static_transformStamped.header.stamp    = rospy.Time.now()
-                self.static_transformStamped.header.frame_id = OBJECT_FRAME_PREFIX_ + str(msg.markers[idx].id)
-                self.static_transformStamped.child_frame_id  = TARGET_FRAME_PREFIX_ + str(msg.markers[idx].id)
-            
-                self.static_transformStamped.transform.translation.x = 0.0
-                self.static_transformStamped.transform.translation.y = 0.0
-                self.static_transformStamped.transform.translation.z = OFFSET_FROM_TARGET
-            
-                quat = tf.transformations.quaternion_from_euler(math.pi, 0.0, math.pi/2.0)
-                self.static_transformStamped.transform.rotation.x = quat[0]
-                self.static_transformStamped.transform.rotation.y = quat[1]
-                self.static_transformStamped.transform.rotation.z = quat[2]
-                self.static_transformStamped.transform.rotation.w = quat[3]
-
-                self.broadcaster.sendTransform(self.static_transformStamped)
-
-                idx += 1
-            return 1
-        else:
-            return -1
-        
-
-
 
     
 if __name__ == "__main__":
