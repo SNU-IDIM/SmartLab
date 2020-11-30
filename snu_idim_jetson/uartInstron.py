dd4e9f7dfb54cc9234b493a37627d7f2d547cbdc
Won-Jae Yun <wonjae.yun@gmail.com>
2020-11-30 19:11:35 +0900

    working... (20.11.30)

--- snu_idim_jetson/uartInstron.py
+++ snu_idim_jetson/uartInstron.py
@@ -41,62 +41,76 @@ class UART:
 class JET:
 	def __init__(self):
 		rospy.init_node('jetson_node')
-		self.phase = 0
-		self.instron_flag = None
+		rospy.Subscriber('instron/command', Float64, self.cmd_instron)
+		self.instron_status_pub = rospy.Publisher("instron/status", String, queue_size=1)
+
 		self.uart = UART()
 
-		self.jetson_publisher = rospy.Publisher("jetson", String, queue_size=1)
-
-		rospy.Subscriber('pc_to_jetson', Float64, self.callback)
+		self.instron_status = 0
+		self.instron_cmd    = 0
 
 		print('[DEBUG] Node initialized !!!')
+
+
+	def pub_status(self, status):
+		status = str(status)
+		self.instron_status_pub.publish(status)
 
 
 	def wait_for_complete(self):
 		rospy.sleep(0.1)
 	
 
-	def callback(self, msg):
-		rospy.loginfo(msg.data)
-		self.start_flag = msg.data
+	def cmd_instron(self, msg):
+		self.instron_cmd = int(msg.data)
+		print('[DEBUG] Instron command: {}'.format(cmd))
 		
-		if (self.start_flag == 10):
-			print('[DEBUG] Experiment - Start')
-			self.jetson_publisher.publish("start")
+		if (self.instron_cmd == 1):
+			print('[DEBUG] Experiment - Initialization')
+			self.instron_status = 1 # 'start'
+			self.pub_status(self.instron_status)
 			self.wait_for_complete()
 
 			print('[DEBUG] Experiment - Gripper close')
-			self.jetson_publisher.publish("gripper close")
+			self.instron_status = 1 # 'gripper close'
+			self.pub_status(self.instron_status)
 			GPIO.output(IN1, GPIO.HIGH)	
-			# time.sleep(10)
 
 			print('[DEBUG] Experiment - Setting')
-			self.jetson_publisher.publish("setting")
+			self.instron_status = 1 # 'setting'
+			self.pub_status(self.instron_status)
 			self.uart.write_data("0\n")
 			self.uart.read_data()
+			time.sleep(5.0)
 
 			print('[DEBUG] Experiment - Setting done')
-			# GPIO.output(IN1, False)
-			time.sleep(5)
-			self.jetson_publisher.publish("done")
+			self.instron_status = 0 # 'done'
+			self.pub_status(self.instron_status)
 
 
-		if (self.start_flag == 20):
+		if (self.instron_cmd == 2):
 			print('[DEBUG] Experiment - Start')
-			self.jetson_publisher.publish("start")
+			self.instron_status = 2 # 'start'
+			self.pub_status(self.instron_status)
 			self.wait_for_complete()
 
 			print('[DEBUG] Experiment - Running')
-			self.jetson_publisher.publish("running")
+			self.instron_status = 2 # 'running'
+			self.pub_status(self.instron_status)
 			self.uart.write_data("1\n")
 			self.uart.read_data()
 
-			print('[DEBUG] Experiment - Finished')
+			print('[DEBUG] Experiment - Gripper open')
 			GPIO.output(IN1, False)
-			time.sleep(5)
-			self.jetson_publisher.publish("done")
+			self.instron_status = 2 # 'gripper open'
+			self.pub_status(self.instron_status)
 
-		if (self.start_flag == 30):
+			print('[DEBUG] Experiment - Finished')
+			time.sleep(5)
+			self.instron_status = 0 # 'done'
+			self.pub_status(self.instron_status)
+
+		if (cmd == 3):
 			rospy.loginfo("Start_flag = %s",self.start_flag)
 			#do something ex) self.open_valve
 			#self.jetson_publisher.publish("finish")
