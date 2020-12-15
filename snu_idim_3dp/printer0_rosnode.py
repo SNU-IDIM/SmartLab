18434221ee1abd682756ab7adbf9e6f3d7364e90
YWJae <wonjae.yun@gmail.com>
2020-12-15 09:58:21 +0900

    3d printer 작업 완료 (20.12.15)

---
+++ snu_idim_3dp/printer0_rosnode.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os, sys
+sys.path.append( os.path.abspath(os.path.join(os.path.dirname(__file__), "../snu_idim_common/src")) )
+
+from DeviceHUB import DeviceHUB
+from Automate3DP import Automate3DP
+
+
+if __name__ == "__main__":
+    device_name = 'printer0'
+    printer0_node = DeviceHUB(device_name='device_name', device_class=Automate3DP(device_name))
