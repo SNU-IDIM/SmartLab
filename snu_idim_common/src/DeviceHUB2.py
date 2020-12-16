303f1e4751a58a4817bacccb9293b9d1f34cf854
YWJae <wonjae.yun@gmail.com>
2020-12-16 21:50:27 +0900

    Device HUB, execution manager 수정중 (윤원재, 20.12.16)

--- snu_idim_common/src/DeviceHUB2.py
+++ snu_idim_common/src/DeviceHUB2.py
@@ -67,12 +67,12 @@ class DeviceHUB:
         # print("[INFO] Received 'status_dict': \n{}".format(self.device_status))
 
 
-    def sendCommand(self, cmd_dict):
+    def manager_sendCommand(self, cmd_dict):
         cmd_json = json.dumps(cmd_dict)
         self.manager_cmd_publisher.publish(cmd_json)
 
 
-    def getStatus(self):
+    def manager_getStatus(self):
         # print("[INFO] Current 'status_dict': \n{}".format(self.device_status))
         return self.device_status
 
