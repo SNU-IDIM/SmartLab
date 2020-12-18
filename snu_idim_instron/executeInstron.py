0edecc24c2af12fd6257858616303370ad45dfde
Won-Jae Yun <wonjae.yun@gmail.com>
2020-12-18 20:22:54 +0900

    Instron status feedback (D.Kim, 20.12,18)

--- snu_idim_instron/executeInstron.py
+++ snu_idim_instron/executeInstron.py
@@ -10,6 +10,10 @@ from autoRun import *
 import time
 import serial
 import sys
+import json
+
+CONNECTION_ONLINE = 1
+
 
 class autoInstron:
 
@@ -24,28 +28,65 @@ class autoInstron:
 		self.state = 0
 		self.autoRun = idimAutomation(folder_dir)
 
+		self.status = dict()
+		self.status = 'connection offline\n'
 
-	def write_data(self):
-		self.serial_port.write("ok\n".encode())
+
+	def write_data(self, msg):
+		self.serial_port.write(msg.encode('utf-8'))
 
 
 	def execute(self, scripts=''):
-		data = ""
+		data = ''
+
 		while True:
 			try:
+
 				if self.serial_port.inWaiting() > 0:
 					data = self.serial_port.readline().decode('utf-8').split('\n')[0]
-					print('[DEBUG] data: {}'.format(data))
-					if data == '0':
-						self.autoRun.execute(scripts[0])
-						self.write_data()
-					elif data == '1':
-						self.autoRun.execute(scripts[1])
-						self.write_data()
+					time.sleep(0.1)
 
+					print('[DEBUG] received data: {}'.format(data))
+					
+					if data == 'online':						# connection online
+						self.status = 'connection online\n'
+						self.write_data(self.status)
+
+					elif data == 'start':						# setup_start
+						self.status = 'progress setup_start\n'
+
+					elif data == 'gripper_close':				# gripper_close
+						self.status = 'progress gripper_close\n'
+
+					elif data == 'setting':						# setting
+						self.autoRun.execute(scripts[0])
+						self.status = 'progress setting\n'
+
+					elif data == 'ready':						# ready
+						self.status = 'progress ready\n'
+
+					elif data == 'experiment_start':			# experiment_start
+						self.status = 'progress experiment_start\n'
+
+					elif data == 'running':						# running
+						self.autoRun.execute(scripts[1])
+						self.status = 'progress running\n'
+
+					elif data == 'gripper_open':				# gripper_open
+						self.status = 'progerss gripper_open\n'
+
+					elif data == 'done':						# done
+						self.status= 'progress done\n'
+
+					print('[DEBUG] sent data: {}'.format(self.status))
+
+				self.write_data(self.status)
 
 			except KeyboardInterrupt:
 				print('keyboard interrupt')
+				self.status = 'connection offline'
+				self.write_data(self.status)
+
 				sys.exit()
 
 			except serial.serialutil.SerialException:
@@ -55,13 +96,17 @@ class autoInstron:
 				time.sleep(5)
 				self.serial_port.open()
 
+			except:
+				self.status = 'connection offline'
+				self.write_data(self.status)
+
 
 
 if __name__=='__main__':
 	print("[DEBUG] Instron Automation Started !!!")
 
 	## Serial communication setting
-	port = 'COM6'
+	port = 'COM5'
 	baud = 115200
 	
 	## Automation program setting
