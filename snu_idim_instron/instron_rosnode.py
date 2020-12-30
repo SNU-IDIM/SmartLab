c76e3d161be0177be47ee5e0d24e450f6ab3deab
Dohyeon Kim <overthree27@gmail.com>
2020-12-30 19:52:15 +0900

    Jetson 결과 저장 (D.Kim, 20.12.30)

--- snu_idim_instron/instron_rosnode.py
+++ snu_idim_instron/instron_rosnode.py
@@ -14,10 +14,10 @@ if __name__ == "__main__":
     device_name = 'instron'
     rospy.init_node(device_name)
     instron_node = DevicePluginToROS(device_name=device_name, device_class=DeviceClass_Instron(device_name=device_name))
-
+    # '''
     rospy.sleep(5.0)
     cmd_dict = dict()
-    cmd_dict['setup'] = 'specimen1'
+    cmd_dict['setup'] = 'specimenb'
 
     instron_node.sendCommand(cmd_dict);     time.sleep(10)
 
@@ -25,8 +25,20 @@ if __name__ == "__main__":
 
         if instron_node.getStatus()['status'] == 'Ready':
             print("execute debugging")
-            cmd_dict['execute'] = 'sepecimen1'
+            cmd_dict['execute'] = 'specimenb'
             del(cmd_dict['setup'])
             instron_node.sendCommand(cmd_dict)
             break
 
+    while True:
+        if instron_node.getStatus()['status'] =='Idle':
+            print("result debugging")
+            cmd_dict['result'] = 'specimenb'
+            del(cmd_dict['execute'])
+            instron_node.sendCommand(cmd_dict)
+            break
+
+        else:
+            continue
+            
+    # '''
