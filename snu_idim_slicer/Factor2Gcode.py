a355673fe6840713582fc36d8ce9641e96f6b07a
Won-Jae Yun <wonjae.yun@gmail.com>
2021-02-24 17:50:05 +0900

    Need to switch server-client (21.02.24)

--- snu_idim_slicer/Factor2Gcode.py
+++ snu_idim_slicer/Factor2Gcode.py
@@ -1,9 +1,8 @@
 #! usr/bin/python
 
-import sys
-import os # os.system
+import sys, os, json
 import zmq
-import json
+
 
 class AutoSlicer():
     def __init__(self, ip='192.168.60.25'):
@@ -16,8 +15,9 @@ class AutoSlicer():
         except:
             print("[ERROR] ZMQ error")
 
-        self.testset_list    = list()
-        self.testset_factors = list()
+        self.testset_dict = dict()
+        self.doe_list     = list()
+        self.doe_factors  = list()
 
     
     def sendRequest(self):
@@ -27,25 +27,26 @@ class AutoSlicer():
         request = self.socket.recv()
 
         request_decoded = request.decode('utf-8')
-        testset_dict = json.loads(request_decoded)
+        self.testset_dict = json.loads(request_decoded)
+        testset_doe = self.testset_dict['doe']
         
 
-        n_test = len(testset_dict['NUMBER'])
+        n_test = len(testset_doe['NUMBER'])
 
-        testset_dict_keys = list(testset_dict.keys())
-        testset_dict_keys.remove('NUMBER')
-        self.testset_factors = testset_dict_keys
-        print(self.testset_factors)
+        testset_doe_keys = list(testset_doe.keys())
+        testset_doe_keys.remove('NUMBER')
+        self.doe_factors = testset_doe_keys
+        print(self.doe_factors)
 
-        n_factors = len(self.testset_factors)
+        n_factors = len(self.doe_factors)
 
-        self.testset_list = list()
+        self.doe_list = list()
         for i in range(n_factors):
-            self.testset_list.append(list())
-            factor = self.testset_factors[i]
+            self.doe_list.append(list())
+            factor = self.doe_factors[i]
 
             for j in range(n_test) :
-                self.testset_list[i].append(testset_dict[factor][str(j)])
+                self.doe_list[i].append(testset_doe[factor][str(j)])
 
 
 
@@ -58,23 +59,21 @@ class AutoSlicer():
         testset_dict[0] = {'Number':'1' , 'infill_line_distance' : '1', 'infill_pattern' : 'lines'}
         testset_dict[1] = {'Number':'2' , 'infill_line_distance' : '6', 'infill_pattern' : 'triangles'}
         '''
-        n_test    = len(self.testset_list[0])
-        n_factors = len(self.testset_factors)
+        n_test_total = len(self.doe_list[0])
+        n_factors    = len(self.doe_factors)
 
         # Change List Type Test Information to Dict Type Test Information
         testset_dict = {}
-        for i in range(n_test):
+        for i in range(n_test_total):
             testset_dict[i] = {'Number':str(i)}
 
             for j in range(n_factors):
-                testset_dict[i][self.testset_factors[j]] = self.testset_list[j][i]
+                testset_dict[i][self.doe_factors[j]] = self.doe_list[j][i]
         
-        print(testset_dict)
-
-        n_test = int(len(testset_dict))
+        n_test_total = int(len(testset_dict))
         n_test_done = 0
 
-        while n_test_done < n_test :
+        while n_test_done < n_test_total :
 
             unit_test_dict = testset_dict[n_test_done]
 
@@ -84,12 +83,11 @@ class AutoSlicer():
             n_factor_setted = 0
             n_factor_unsetted = len(unit_test_dict)
 
-            HOME_DIR = os.getenv("HOME")
-            CURA_ENGINE_DIR = os.path.join(HOME_DIR, 'CuraEngine/build/CuraEngine')
+            CURA_ENGINE_DIR = os.path.join(os.getenv("HOME"), 'CuraEngine/build/CuraEngine')
             STL_FILE_DIR = "specimen_3T.stl"
             JSON_FILE_DIR = 'anet.def.json'
-            TEST_HEADER_ID = 'demo'
-            SAVE_DIR = os.path.join(HOME_DIR, '.octoprint/uploads', TEST_HEADER_ID)
+            TEST_HEADER_ID = self.testset_dict['header']['header_id']
+            SAVE_DIR = os.path.join(os.getenv("HOME"), '.octoprint/uploads', TEST_HEADER_ID)
 
             try:
                 os.makedirs(SAVE_DIR)
@@ -102,12 +100,9 @@ class AutoSlicer():
             option_save = ' -o "{}"'.format(os.path.join(SAVE_DIR, "{}_{}.gcode".format(TEST_HEADER_ID, n_test_done)))
             option_stl  = ' -l "{}"'.format(STL_FILE_DIR)
 
-            command = command + option_json
+            command = command + option_json + option_save + option_stl
 
             while n_factor_setted < n_factor_unsetted :
-
-                #1. Converting Dictionary to List Datatype --> Selected Path
-                ## Add "IF command" to change Children Factor when high class Factor Input##
 
                 factor_dict_keys   = list(unit_test_dict.keys())
                 factor_dict_values = list(unit_test_dict.values())
@@ -117,18 +112,11 @@ class AutoSlicer():
 
                 n_factor_setted += 1
 
-            n_test_done += 1
-            
-            command = command + option_save + option_stl
-            print("[DEBUG] Slicer command: \n{}".format(command))
 
-            os.system(command) #Operate
-            
-
+            print("[DEBUG] Slicer command(): \n{}".format(n_test_done, command))
+            os.system(command)
             del command
-
-        print(testset_dict)
-
+            n_test_done += 1
 
 
 if __name__ == "__main__":
@@ -136,9 +124,3 @@ if __name__ == "__main__":
     slicer = AutoSlicer(ip='192.168.0.40')
     slicer.sendRequest()
     slicer.execute()
-
-    # TestInfo, Request_TestInfo_Keys = zmq_client()
-    # print(TestInfo, Request_TestInfo_Keys)
-    # Factor2Gcode(TestInfo, Request_TestInfo_Keys)
-
-    # print('TEST Information Successfully Changed To Gcode:)')
