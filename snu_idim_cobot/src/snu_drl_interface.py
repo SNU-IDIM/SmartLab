fee413f4729703160754b016083b292c4c1d094d
WonJae Yun <wonjae.yun@gmail.com>
2021-03-08 16:43:05 +0900

    AMR 코드 추가, 이동 필요 (210308)

--- snu_idim_cobot/src/snu_drl_interface.py
+++ snu_idim_cobot/src/snu_drl_interface.py
@@ -19,8 +19,10 @@ class DeviceClass_Cobot():
         self.listener = tf.TransformListener()
         rospy.Subscriber("cobot/command", String, self.pnp_cb, queue_size=1)
         rospy.Subscriber("dsr/state", RobotState, self.dsr_state_cb, queue_size=1)
+        rospy.Subscriber("/R_001/robot_state", SysconRobotState, self.amr_state_cb, queue_size=1)
         
-        self.status_pub = rospy.Publisher("cobot/status", String, queue_size=1)
+        self.amr_status_pub = rospy.Publisher("amr/status", String, queue_size=1)
+        self.cobot_status_pub = rospy.Publisher("cobot/status", String, queue_size=1)
         self.image_sub = rospy.Subscriber("/R_001/camera/color/image_raw",Image,self.vision_cb)
         self.vision_pub = rospy.Publisher("vision_2d_flag",Int32, queue_size=1)
         self.gripper_pub = rospy.Publisher("PC_to_GRIPPER", String, queue_size=1)
@@ -97,24 +99,43 @@ class DeviceClass_Cobot():
 
     def publishStatus(self):
         msg_json = json.dumps(self.status)
-        self.status_pub.publish(msg_json)
-        print("\n==============================================================")
-        print("[DEBUG] device_type: {}".format(self.status['device_type']))
-        print("[DEBUG] device_name: {}".format(self.status['device_name']))
-        print("[DEBUG] status: {}".format(self.status['status']))
-        print("[DEBUG] current_work: {}".format(self.status['current_work']))
-        print("[DEBUG] recent_work: {}".format(self.status['recent_work']))
-        print("[DEBUG] posj: {}".format(self.status['posj']))
-        print("[DEBUG] posx: {}".format(self.status['posx']))
-        print("[DEBUG] force: {}".format(self.status['force']))
-        print("[DEBUG] torque: {}".format(self.status['torque']))
-        print("[DEBUG] compressor: {}".format(self.status['compressor']))
-        print("[DEBUG] toolchanger: {}".format(self.status['toolchanger']))
-        print("[DEBUG] universal_jig_x: {}".format(self.status['universal_jig_x']))
-        print("[DEBUG] universal_jig_y: {}".format(self.status['universal_jig_y']))
-        print("[DEBUG] gripper_type: {}".format(self.status['gripper_type']))
-        print("[DEBUG] gripper_state_suction: {}".format(self.status['gripper_state_suction']))
-        print("[DEBUG] gripper_state_mech: {}".format(self.status['gripper_state_mech']))
+        self.cobot_status_pub.publish(msg_json)
+        # print("\n==============================================================")
+        # print("[DEBUG] device_type: {}".format(self.status['device_type']))
+        # print("[DEBUG] device_name: {}".format(self.status['device_name']))
+        # print("[DEBUG] status: {}".format(self.status['status']))
+        # print("[DEBUG] current_work: {}".format(self.status['current_work']))
+        # print("[DEBUG] recent_work: {}".format(self.status['recent_work']))
+        # print("[DEBUG] posj: {}".format(self.status['posj']))
+        # print("[DEBUG] posx: {}".format(self.status['posx']))
+        # print("[DEBUG] force: {}".format(self.status['force']))
+        # print("[DEBUG] torque: {}".format(self.status['torque']))
+        # print("[DEBUG] compressor: {}".format(self.status['compressor']))
+        # print("[DEBUG] toolchanger: {}".format(self.status['toolchanger']))
+        # print("[DEBUG] universal_jig_x: {}".format(self.status['universal_jig_x']))
+        # print("[DEBUG] universal_jig_y: {}".format(self.status['universal_jig_y']))
+        # print("[DEBUG] gripper_type: {}".format(self.status['gripper_type']))
+        # print("[DEBUG] gripper_state_suction: {}".format(self.status['gripper_state_suction']))
+        # print("[DEBUG] gripper_state_mech: {}".format(self.status['gripper_state_mech']))
+
+
+    def amr_state_cb(self, msg):
+        
+        pose = {
+            'x': msg.pose.x,
+            'y': msg.pose.y,
+            'theta': msg.pose.theta,
+        }
+
+        amr_status = dict()
+        amr_status['device_type'] = 'AMR'
+        amr_status['device_name'] = 'R_001/amr'
+        amr_status['status'] = msg.workstate
+        amr_status['current_work'] = None
+        amr_status['recent_work'] = None
+
+        msg_json = json.dumps(amr_status)
+        self.amr_status_pub.publish(msg_json)
 
 
     '''
