ac3c333104c187a80140cd78baf9c3086b654e8d
YWJae <wonjae.yun@gmail.com>
2021-04-25 01:41:55 +0900

    SQLHelper 수정 (21.04.25, WJ)

--- snu_idim_gui/sqlHelper.py
+++ snu_idim_gui/sqlHelper.py
@@ -85,10 +85,6 @@ class SqlHelper():
         os.remove(tablename + '.csv')
 
     def execute_sql(self, sql=''):
-        """执行sql语句，针对读操作返回结果集
-            args：
-                sql  ：sql语句
-        """
         try:
             self.cur.execute(sql)
             records = self.cur.fetchall()
@@ -98,8 +94,6 @@ class SqlHelper():
             print(error)
 
     def execute_sql_file(self, sql):
-        """ 执行 sql'文件中的sql
-        """
         if not os.path.isfile(sql):
             raise 'invalid input file path: %s' % sql
         stmts = []
@@ -143,8 +137,6 @@ class SqlHelper():
             raise error
 
     def execute_commit(self,sql=''):
-        """执行数据库sql语句，针对更新,删除,事务等操作失败时回滚
-        """
         try:
             self.cur.execute(sql)
             self.con.commit()
@@ -176,7 +168,6 @@ class SqlHelper():
             values_sql = f'{tuple(values)}'
             if values_sql[-2:].find(',') != -1:
                 values_sql = f'{tuple(values)}'[:-2] + ')'
-                print("@@@@@@@@@@@@@@@@@@")
 
         sql = 'INSERT INTO {} ({}) VALUES {}'.format(tablename, attrs_sql, values_sql)
         print('[SQL] {}'.format(sql))
@@ -216,74 +207,9 @@ class SqlHelper():
 
         return data
 
-    def insert_many(self, table, attrs = None, values = None, datalist = None):
-        """插入多条数据
-            example：
-                table='test_mysqldb'
-                key = ["id" ,"name", "age"]
-                value = [[101, "liuqiao", "25"], [102,"liuqiao1", "26"], [103 ,"liuqiao2", "27"], [104 ,"liuqiao3", "28"]]
-                mydb.insert_many(table, key, value)
-                data = [{"id":101,"name":"liuqiao","age":"25"},{"id":102,"name":"liuqiao1", "age":"26"}]
-                nydb.insert_many(table, datalist=data)
-        """
-        if datalist is not None:
-            if not isinstance(datalist, (list, tuple)):
-                self.insert(table, datalist)
-                return
-
-            if len(datalist) == 1:
-                self.insert(table, datalist[0])
-                return
-
-            keys = datalist[0].keys()
-            re_data = []
-            cand_data = []
-            for d in datalist:
-                if not len(d.keys()) == len(keys):
-                    re_data.append(d)
-                    continue
-                keys_same = True
-                for k in keys:
-                    if not k in d.keys():
-                        re_data.append(d)
-                        keys_same = False
-                        continue
-                if keys_same:
-                    cand_data.append(d)
-
-            cand_list = []
-            for d in cand_data:
-                d_list = []
-                for k in keys:
-                    d_list.append(d[k])
-                cand_list.append(d_list)
-
-            self.insert_many(table, keys, cand_list)
-
-            if len(re_data):
-                self.insert_many(table, datalist = re_data)
-
-        if attrs and values:
-            values_sql = ['%s' for v in attrs]
-            attrs_sql = '('+','.join(attrs)+')'
-            values_sql = ' values('+','.join(values_sql)+')'
-            sql = 'insert into %s'% table
-            sql = sql + attrs_sql + values_sql
-            #print('insert_many:'+sql)
-            try:
-                for i in range(0,len(values),20000):
-                    self.cur.executemany(sql,values[i:i+20000])
-                    self.con.commit()
-            except pymysql.Error as e:
-                self.con.rollback()
-                error = 'insert_many executemany failed! ERROR (%s): %s' %(e.args[0],e.args[1])
-                print(error)
 
     def delete(self, tablename, cond_dict):
-        """删除数据
-            args：
-                tablename  ：表名字
-                cond_dict  ：删除条件字典
+        """
             example：
                 params = {"name" : "caixinglong", "age" : "38"}
                 mydb.delete(table, params)
@@ -300,11 +226,7 @@ class SqlHelper():
         return self.execute_commit(sql)
 
     def update(self, tablename, attrs_dict, cond_dict):
-        """更新数据
-            args：
-                tablename  ：表名字
-                attrs_dict  ：更新属性键值对字典
-                cond_dict  ：更新条件字典
+        """
             example：
                 params = {"name" : "caixinglong", "age" : "38"}
                 cond_dict = {"name" : "liuqiao", "age" : "18"}
@@ -325,116 +247,36 @@ class SqlHelper():
         #print(sql)
         return self.execute_commit(sql)
 
-    def update_many(self, table, conkeys, attrs = None, values = None, datalist = None):
-        """更新多条数据, 有重复则
-            args：
-                tablename  : 表名字
-                attrs      : 属性键
-                conkeys    : 条件属性键
-                values     : 所有属性值
-                datalist   : dict list of data
-            example：
-                table='test_mysqldb'
-                keys = ["name", "age"]
-                conkeys = ["id"]
-                values = [["liuqiao", "25", 101], ["liuqiao1", "26", 102], ["liuqiao2", "27", 103], ["liuqiao3", "28", 104]]
-                mydb.update_many(table, conkeys, keys, values)
-        """
-
-        if datalist is not None:
-            if not isinstance(datalist, (list, tuple)):
-                print('update_many Error: datalist only accept list or tuple, but get ', type(datalist))
-                return
-
-            if len(datalist) == 1:
-                attrs_dict = {k: v for k, v in datalist[0].items() if k not in conkeys and v is not None}
-                cond_dict = {k: v for k, v in datalist[0].items() if k in conkeys and v is not None}
-                self.update(table, attrs_dict, cond_dict)
-                return
-
-            keys = datalist[0].keys()
-            re_data = []
-            cand_data = []
-            for d in datalist:
-                if not len(d.keys()) == len(keys):
-                    re_data.append(d)
-                    continue
-                keys_same = True
-                for k in keys:
-                    if not k in d.keys():
-                        re_data.append(d)
-                        keys_same = False
-                        continue
-                if keys_same:
-                    cand_data.append(d)
-
-            cand_list = []
-            attr_keys = [k for k in keys if k not in conkeys]
-            for d in cand_data:
-                d_list = []
-                for k in attr_keys:
-                    d_list.append(d[k])
-                for k in conkeys:
-                    d_list.append(d[k])
-                cand_list.append(d_list)
-
-            self.update_many(table, conkeys = conkeys, attrs = attr_keys, values = cand_list)
-
-            if len(re_data):
-                self.update_many(table, conkeys = conkeys, datalist = re_data)
-
-        if attrs is not None and values is not None:
-            attrs_list = [a + '=(%s)' for a in attrs]
-            attrs_sql = ','.join(attrs_list)
-            cond_list = [c + '=(%s)' for c in conkeys]
-            cond_sql = ' and '.join(cond_list)
-            sql = "UPDATE %s SET %s where %s" % (table, attrs_sql, cond_sql)
-            try:
-                for i in range(0,len(values),20000):
-                    self.cur.executemany(sql, values[i:i+20000])
-                    self.con.commit()
-            except pymysql.Error as e:
-                self.con.rollback()
-                error = 'update_many executemany failed! ERROR (%s): %s' %(e.args[0],e.args[1])
-                print(error)
 
     def drop_table(self, tablename):
-        """删除数据库表
-            args：
-                tablename  ：表名字
-        """
-        sql = "DROP TABLE  %s" % tablename
+        sql = "DROP TABLE {}".format(tablename)
         self.execute_commit(sql)
+
 
     def delete_table(self, tablename):
-        """清空数据库表
-            args：
-                tablename  ：表名字
-        """
-        sql = "DELETE FROM %s" % tablename
+        sql = "DELETE FROM {}".format(tablename)
         self.execute_commit(sql)
 
+
     def is_exist_schema(self, database):
-        result, = self.select("information_schema.SCHEMATA", "count(*)",["schema_name = '%s'" % database]);
+        result, = self.select("information_schema.SCHEMATA", "count(*)",["schema_name = '%s'" % database])
         return result > 0
 
+
     def is_exist_table(self, tablename):
-        """判断数据表是否存在
-            args：
-                tablename  ：表名字
-            Return:
-                存在返回True，不存在返回False
-        """
         result, = self.select("information_schema.tables","count(*)",["table_name = '%s'" % tablename, "table_schema = '%s'" % self.database])
         return result > 0
+
 
     def is_exist_table_column(self, tablename, column_name):
         result, = self.select("information_schema.columns","count(*)",["table_name = '%s'" % tablename, "column_name = '%s'" % column_name, "table_schema = '%s'" % self.database])
         return result > 0
 
+
     def is_exist_table_rows(self, tablename, clauses):
-        result = self.select(tablename, '*', conds = clauses)
+        result = self.select(tablename, '*', conds=clauses)
         return False if result is None or len(result) == 0 else len(result[0]) > 0
+
 
     def get_table_columns(self, tablename):
         return self.select("information_schema.columns", "column_name", ["table_schema = '%s'" % self.database, "table_name = '%s'" % tablename])
