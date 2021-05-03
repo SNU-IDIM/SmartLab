4269229fa2e93d7aef0555a28fe91770feb7fd2f
YWJae <wonjae.yun@gmail.com>
2021-05-03 23:27:11 +0900

    GUI 작업 - Frame으로 변경, Streaming 추가 (21.05.03, WJ)

--- snu_idim_gui/SmartLab_GUI.py
+++ snu_idim_gui/SmartLab_GUI.py
@@ -4,10 +4,28 @@ from PyQt5.QtWidgets import *
 from PyQt5 import uic, QtCore
 from PyQt5.QtGui import *
 from PyQt5.QtWebEngineWidgets import *
-from PyQt5.QtCore import QUrl
+from PyQt5.QtCore import QUrl, QThread, pyqtSignal, pyqtSlot, Qt
 from SqlHelper import SqlHelper
 import json
 import ast
+import cv2
+
+
+class Thread(QThread):
+    changePixmap = pyqtSignal(QImage)
+
+    def run(self):
+        cap = cv2.VideoCapture(0)
+        while True:
+            ret, frame = cap.read()
+            if ret:
+                # https://stackoverflow.com/a/55468544/6622587
+                rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                h, w, ch = rgbImage.shape
+                bytesPerLine = ch * w
+                convertToQtFormat = QImage(rgbImage.data, w, h, bytesPerLine, QImage.Format_RGB888)
+                p = convertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
+                self.changePixmap.emit(p)
 
 
 class SmartLAB_GUI(QMainWindow, QDialog):
@@ -28,11 +46,23 @@ class SmartLAB_GUI(QMainWindow, QDialog):
         self.btn_doe_create.clicked.connect(self.cb_btn_doe_create)
         self.cbx_control.currentIndexChanged.connect(self.cb_cbx_control)
 
+        # create a label
+        # self.label = QLabel(self)
+        # self.label.move(280, 120)
+        # self.label.resize(640, 480)
+
+        th = Thread(self)
+        th.changePixmap.connect(self.setImage)
+        th.start()
+
         # self.widget_streaming.setStyleSheet("background-color: rgb(84, 84, 84);")
         # self.webview = QWebEngineView(self.widget_streaming)
         # self.webview.setUrl(QUrl("https://www.youtube.com/embed/t67_zAg5vvI?autoplay=1"))
         # self.webview.setGeometry(0, 0, 500, 300)
 
+    @pyqtSlot(QImage)
+    def setImage(self, image):
+        self.label.setPixmap(QPixmap.fromImage(image))
     
     def updateStatus(self):
         '''
--- snu_idim_gui/test.py
+++ snu_idim_gui/test.py
@@ -1,37 +1,47 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget
-
+import cv2
 import sys
+from PyQt5.QtWidgets import  QWidget, QLabel, QApplication
+from PyQt5.QtCore import QThread, Qt, pyqtSignal, pyqtSlot
+from PyQt5.QtGui import QImage, QPixmap
 
-from random import randint
+class Thread(QThread):
+    changePixmap = pyqtSignal(QImage)
+
+    def run(self):
+        cap = cv2.VideoCapture(0)
+        while True:
+            ret, frame = cap.read()
+            if ret:
+                # https://stackoverflow.com/a/55468544/6622587
+                rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                h, w, ch = rgbImage.shape
+                bytesPerLine = ch * w
+                convertToQtFormat = QImage(rgbImage.data, w, h, bytesPerLine, QImage.Format_RGB888)
+                p = convertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
+                self.changePixmap.emit(p)
 
 
-class AnotherWindow(QWidget):
-    """
-    This "window" is a QWidget. If it has no parent, it
-    will appear as a free-floating window as we want.
-    """
+class App(QWidget):
     def __init__(self):
         super().__init__()
-        layout = QVBoxLayout()
-        self.label = QLabel("Another Window % d" % randint(0,100))
-        layout.addWidget(self.label)
-        self.setLayout(layout)
+        [...]
+        self.initUI()
 
+    @pyqtSlot(QImage)
+    def setImage(self, image):
+        self.label.setPixmap(QPixmap.fromImage(image))
 
-class MainWindow(QMainWindow):
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.resize(1800, 1200)
+        # create a label
+        self.label = QLabel(self)
+        self.label.move(280, 120)
+        self.label.resize(640, 480)
+        th = Thread(self)
+        th.changePixmap.connect(self.setImage)
+        th.start()
+        self.show()
 
-    def __init__(self):
-        super().__init__()
-        self.w = AnotherWindow()
-        self.button = QPushButton("Push for Window")
-        self.button.clicked.connect(self.show_new_window)
-        self.setCentralWidget(self.button)
-
-    def show_new_window(self, checked):
-        self.w.show()
-
-
-app = QApplication(sys.argv)
-w = MainWindow()
-w.show()
-app.exec_()
+App()
