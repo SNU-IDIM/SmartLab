6311c3c8b27e034e3b45d6e7b121e05285e6b477
KWLee <ksksooo@snu.ac.kr>
2021-05-13 15:15:43 +0900

    GUI 작업(table to text, graph 추가, DoE 수정, Detailed information, tab 수정) (21.05.13, KW)

--- snu_idim_gui/SmartLab_GUI.py
+++ snu_idim_gui/SmartLab_GUI.py
@@ -6,7 +6,9 @@ import os
 import sys
 import time
 from threading import Thread
-
+import base64
+from PIL import Image
+import io
 import cv2
 from PyQt5 import QtCore, uic
 from PyQt5.QtCore import Qt, QThread, QUrl, pyqtSignal, pyqtSlot
@@ -76,7 +78,7 @@ class cam360(QThread):
 
     def run(self):
         url = 'https://www.youtube.com/watch?v=avmSkeJwT0o'
-
+        '''
         while True:
             vPafy = pafy.new(url)
             play = vPafy.getbest(preftype="mp4")
@@ -91,7 +93,7 @@ class cam360(QThread):
                 convertToQtFormat = QImage(rgbImage.data, w, h, bytesPerLine, QImage.Format_RGB888)
                 p = convertToQtFormat.scaled(360, 240, Qt.KeepAspectRatio)
                 self.changePixmap.emit(p)
-
+        '''
 
 
 class SmartLAB_GUI(QMainWindow, QDialog):
@@ -102,9 +104,9 @@ class SmartLAB_GUI(QMainWindow, QDialog):
 
         self.init_flag = False
 
-        # self.sql = SqlHelper(host='192.168.60.21', username='wjYun', password='0000', port=3306, database='SmartLab')
-        # self.thread_server = Thread(target=self.updateStatus)
-        # self.thread_server.start()
+        self.sql = SqlHelper(host='192.168.0.88', username='wjYun', password='0000', port=3306, database='SmartLab')
+        self.thread_server = Thread(target=self.updateStatus)
+        self.thread_server.start()
 
         self.smartlab = SmartLabClient(ip='192.168.60.21')
         self.streaming = Cam_Streaming_Client(ip='192.168.60.21', cam_list=['cobot'])
@@ -124,12 +126,14 @@ class SmartLAB_GUI(QMainWindow, QDialog):
         self.btn_exp_export.clicked.connect(self.cb_btn_exp_export)
         self.cbx_control.currentIndexChanged.connect(self.cb_cbx_control)
         self.btn_doe_apply.clicked.connect(self.cb_btn_doe_apply)
+        self.btn_exp_detail.clicked.connect(self.cb_btn_exp_detail)
 
         # create a label
         # self.label = QLabel(self)
         # self.label.move(280, 120)
         # self.label.resize(640, 480)
 
+        self.showlogo()
 
         #The camera thread setting function
         # th = QtThread(self)
@@ -154,8 +158,6 @@ class SmartLAB_GUI(QMainWindow, QDialog):
         # self.webview.setUrl(QUrl("https://www.youtube.com/watch?v=avmSkeJwT0o"))
         # self.webview.setGeometry(0, 0, 500, 300)
 
-
-
     
     def updateDeviceTable(self, device_status):
         for device_id in device_status:
@@ -164,61 +166,44 @@ class SmartLAB_GUI(QMainWindow, QDialog):
                 test_info = json.loads(test_info)
             
                 if   device_id.find('amr') != -1:
-                    self.table_amr.setColumnCount(1)
-                    self.table_amr.setRowCount(len(test_info))
-                    self.table_amr.setVerticalHeaderLabels((list(test_info.keys())))
-                    for row, key in enumerate(test_info):
-                        newitem = QTableWidgetItem(str(test_info[key]))
-                        self.table_amr.setItem(row, 0, newitem)
+                    self.AMR_status.setText(str(test_info['status']))
+                    self.AMR_SN.setText(str(test_info['subject_name'])) ####TBD####
+                    self.AMR_task.setText(str(test_info['current_work'])) ####TBD####
                 elif device_id.find('cobot') != -1:
-                    self.table_cobot.setColumnCount(1)
-                    self.table_cobot.setRowCount(len(test_info))
-                    self.table_cobot.setVerticalHeaderLabels((list(test_info.keys())))
-                    for row, key in enumerate(test_info):
-                        newitem = QTableWidgetItem(str(test_info[key]))
-                        self.table_cobot.setItem(row, 0, newitem)
+                    self.Cobot_comp.setText(str(test_info["compressor"]))
+                    self.Cobot_current.setText(str(test_info['pose'])) 
+                    self.Cobot_gripper.setText(str(test_info['gripper_type']))
+                    self.Cobot_status.setText(str(test_info['recent_work'])) ####TBD####
                 elif device_id.find('MS') != -1:
-                    self.table_omm.setColumnCount(1)
-                    self.table_omm.setRowCount(len(test_info))
-                    self.table_omm.setVerticalHeaderLabels((list(test_info.keys())))
-                    for row, key in enumerate(test_info):
-                        newitem = QTableWidgetItem(str(test_info[key]))
-                        self.table_omm.setItem(row, 0, newitem)
+                    self.OMM_SN.setText(str(test_info["recent_work"]))####TBD####
+                    self.OMM_status.setText(str(test_info['status']))
                 elif device_id.find('instron') != -1:
-                    self.table_instron.setColumnCount(1)
-                    self.table_instron.setRowCount(len(test_info))
-                    self.table_instron.setVerticalHeaderLabels((list(test_info.keys())))
-                    for row, key in enumerate(test_info):
-                        newitem = QTableWidgetItem(str(test_info[key]))
-                        self.table_instron.setItem(row, 0, newitem)
+                    self.Instron_SN.setText(str(test_info["subject_name"]))
+                    self.Instron_status.setText(str(test_info['status']))
                 elif device_id.find('printer1') != -1:
-                    self.table_printer_1.setColumnCount(1)
-                    self.table_printer_1.setRowCount(len(test_info))
-                    self.table_printer_1.setVerticalHeaderLabels((list(test_info.keys())))
-                    for row, key in enumerate(test_info):
-                        newitem = QTableWidgetItem(str(test_info[key]))
-                        self.table_printer_1.setItem(row, 0, newitem)
+                    self.TDP1_SN.setText(str(test_info["subject_name"]))
+                    self.TDP1_status.setText(str(test_info['status']))
+                    self.TDP1_bed.setText(str(test_info['bed_temperature']))
+                    self.TDP1_nozzle.setText(str(test_info['nozzle_temperature']))
+                    self.TDP1_time.setText(str(test_info['time_left']))
                 elif device_id.find('printer2') != -1:
-                    self.table_printer_2.setColumnCount(1)
-                    self.table_printer_2.setRowCount(len(test_info))
-                    self.table_printer_2.setVerticalHeaderLabels((list(test_info.keys())))
-                    for row, key in enumerate(test_info):
-                        newitem = QTableWidgetItem(str(test_info[key]))
-                        self.table_printer_2.setItem(row, 0, newitem)
+                    self.TDP2_SN.setText(str(test_info["subject_name"]))
+                    self.TDP2_status.setText(str(test_info['status']))
+                    self.TDP2_bed.setText(str(test_info['bed_temperature']))
+                    self.TDP2_nozzle.setText(str(test_info['nozzle_temperature']))
+                    self.TDP2_time.setText(str(test_info['time_left']))
                 elif device_id.find('printer3') != -1:
-                    self.table_printer_3.setColumnCount(1)
-                    self.table_printer_3.setRowCount(len(test_info))
-                    self.table_printer_3.setVerticalHeaderLabels((list(test_info.keys())))
-                    for row, key in enumerate(test_info):
-                        newitem = QTableWidgetItem(str(test_info[key]))
-                        self.table_printer_3.setItem(row, 0, newitem)
+                    self.TDP3_SN.setText(str(test_info["subject_name"]))
+                    self.TDP3_status.setText(str(test_info['status']))
+                    self.TDP3_bed.setText(str(test_info['bed_temperature']))
+                    self.TDP3_nozzle.setText(str(test_info['nozzle_temperature']))
+                    self.TDP3_time.setText(str(test_info['time_left']))
                 elif device_id.find('printer4') != -1:
-                    self.table_printer_4.setColumnCount(1)
-                    self.table_printer_4.setRowCount(len(test_info))
-                    self.table_printer_4.setVerticalHeaderLabels((list(test_info.keys())))
-                    for row, key in enumerate(test_info):
-                        newitem = QTableWidgetItem(str(test_info[key]))
-                        self.table_printer_4.setItem(row, 0, newitem)
+                    self.TDP4_SN.setText(str(test_info["subject_name"]))
+                    self.TDP4_status.setText(str(test_info['status']))
+                    self.TDP4_bed.setText(str(test_info['bed_temperature']))
+                    self.TDP4_nozzle.setText(str(test_info['nozzle_temperature']))
+                    self.TDP1_time.setText(str(test_info['time_left']))
             except:
                 pass
 
@@ -260,6 +245,7 @@ class SmartLAB_GUI(QMainWindow, QDialog):
                     self.table_exp_info.setColumnCount(n_col)
                     # vbox = QVBoxLayout(self)
                     # vbox.addWidget(self.table_exp_info)
+                    print((list(test_info[0].keys())))
                     self.table_exp_info.setHorizontalHeaderLabels((list(test_info[0].keys())))
                 
                     for row, item_list in enumerate(test_info):
@@ -281,6 +267,51 @@ class SmartLAB_GUI(QMainWindow, QDialog):
                 print("[ERROR] SmartLab information update error !!!")
 
             time.sleep(1.0)
+
+    def cb_btn_exp_detail(self):
+        self.graphicsItem = None
+        sql = SqlHelper(host='192.168.0.88', username='wjYun', password='0000', port=3306, database='SmartLab')
+
+        selectedRow = self.table_exp_info.currentRow()
+        if selectedRow != -1:
+            print('selectedRow', selectedRow)
+            # selectedIdx = self.table_exp_info.selectedIndexes()
+
+            selected_item = self.table_exp_info.item(selectedRow, 0)
+            selected_subject_name = selected_item.text()
+            # selected_subject_name = 'calb'  ##for debug
+            factor_list = list()
+            factor_status_list = list()
+            fields = ['Status', 'Thickness', 'Length', 'Width', 'E_modulus', 'U_stress', 'Plot']
+
+            factors = self.smartlab_cmd['setup_doe']['factors']
+            factor_count = len(factors)
+            for i in range(factor_count):
+                fields.append(factors[i]['factor_name'])
+                factor_list.append(factors[i]['factor_name'])
+                
+            selected_item_info = sql.select(tablename='result', fields=fields, conds="subject_name=\'{}\'".format(selected_subject_name))[0]
+            sql.close()
+
+            Plot = selected_item_info['Plot']
+            selected_item_info.pop('Plot')
+            fields.remove('Plot')
+
+            self.information_widget.setRowCount(len(fields))
+            self.information_widget.setVerticalHeaderLabels(fields)
+            for index, key in enumerate(fields):
+                item = selected_item_info[key]
+                print(item)
+                QItem = QTableWidgetItem(str(item))
+                self.information_widget.setItem(index, 0, QItem)
+
+            if Plot is not None:
+                Graph_binary    = base64.b64decode(Plot)
+                image           = Image.open(io.BytesIO(Graph_binary))
+                qPixmapVar = QPixmap()
+                qPixmapVar.loadFromData(Graph_binary)
+                qPixmapVar = qPixmapVar.scaled(self.graph.width(), self.graph.height())
+                self.graph.setPixmap(qPixmapVar)
 
     def cb_btn_exp_export(self):
         print("[DEBUG] 'Experiment - Export' button clicked !!! (TBD)")
@@ -339,6 +370,17 @@ class SmartLAB_GUI(QMainWindow, QDialog):
     def cb_cbx_control(self):
         self.smartlab_cmd['test_mode'] = self.cbx_control.currentText()
         print("[DEBUG] Execution mode: {}".format(self.smartlab_cmd['test_mode']))
+
+    def showlogo(self):
+        SNU_image = QPixmap()
+        SNU_image.load('./src/snu.jpg')
+        SNU_image = SNU_image.scaled(self.logo_SNU.width(), self.logo_SNU.height())
+        IDIM_image = QPixmap()
+        IDIM_image.load('./src/idim.png')
+        IDIM_image = IDIM_image.scaled(self.logo_IDIM.width(), self.logo_IDIM.height())
+
+        self.logo_IDIM.setPixmap(IDIM_image)
+        self.logo_SNU.setPixmap(SNU_image)
 
 
     # def cbx_device_cb(self):
@@ -400,31 +442,36 @@ class DOE_Window(QDialog) :
         options = list()
         if self.checkBox_linedistance.isChecked():
             factor=dict()
-            factor['factor_name'] = self.checkBox_linedistance.text()
+            #factor['factor_name'] = self.checkBox_linedistance.text()
+            factor['factor_name'] = 'infill_line_distance'
             factor['factor_range'] = self.txt_linedistance_range.text().split(',')
             factors.append(factor)
             options.append(self.txt_linedistance_option.text().split(','))
         if self.checkBox_linewidth.isChecked():
             factor=dict()
-            factor['factor_name'] = self.checkBox_linewidth.text()
+            #factor['factor_name'] = self.checkBox_linewidth.text()
+            factor['factor_name'] = 'infill_line_width'
             factor['factor_range'] = self.txt_linewidth_range.text().split(',')
             factors.append(factor)
             options.append(self.txt_linewidth_option.text().split(','))
         if self.checkBox_layerheight.isChecked():
             factor=dict()
-            factor['factor_name'] = self.checkBox_layerheight.text()
+            #factor['factor_name'] = self.checkBox_layerheight.text()
+            factor['factor_name'] = 'layer_height'
             factor['factor_range'] = self.txt_layerheight_range.text().split(',')
             factors.append(factor)
             options.append(self.txt_layerheight_option.text().split(','))
         if self.checkBox_rasterangle.isChecked():
             factor=dict()
-            factor['factor_name'] = self.checkBox_rasterangle.text()
+            #factor['factor_name'] = self.checkBox_rasterangle.text()
+            factor['factor_name'] = 'infill_angles'
             # factor['factor_range'] = self.txt_rasterangle_range.text()
             factors.append(factor)
             options.append(self.txt_rasterangle_option.text().split('/'))
         if self.checkBox_infillpatern.isChecked():
             factor=dict()
-            factor['factor_name'] = self.checkBox_linedistance.text()
+            #factor['factor_name'] = self.checkBox_linedistance.text()
+            factor['factor_name'] = 'infill_pattern'
             # factor['factor_range'] = self.cbx_infill_pattern.currentText()
             factors.append(factor)
             # options.append(self.txt_linewidth_option.text())
@@ -432,6 +479,7 @@ class DOE_Window(QDialog) :
         self.doe['option'] = options
 
         print(self.doe)
+
 
 
 if __name__ == "__main__":
